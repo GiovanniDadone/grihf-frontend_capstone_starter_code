8d3b089b03a045f1b579714ba577afffe3107808
GiovanniDadone <dadone.giovanni94@gmail.com>
2024-10-13 23:53:40 +0200

    faulty landing page eliminated

--- grihf-frontend_capstone_starter_code/capstoneproject/src/Pages/Home/Home.tsx
+++ grihf-frontend_capstone_starter_code/capstoneproject/src/Pages/Home/Home.tsx
@@ -1,37 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import LandingPage from './LandingPage';
-
-const Home: React.FC = () => {
-  const [showLandingPage, setShowLandingPage] = useState(true);
-
-  useEffect(() => {
-    // Check sessionStorage on component mount
-    const hasSeenLandingPage = sessionStorage.getItem('landingPageShown');
-    // Conditionally set showLandingPage based on the flag
-    setShowLandingPage(hasSeenLandingPage ? true : false); 
-
-    // Set the flag in sessionStorage if it hasn't been set yet
-    if (!hasSeenLandingPage) {
-      sessionStorage.setItem('landingPageShown', 'true'); 
-    }
-  }, []);
-
-  const handleGetStarted = () => {
-    setShowLandingPage(false);
-  };
+const Home = () => {
 
   return (
     <div>
-      {showLandingPage ? (
-        <LandingPage onGetStarted={handleGetStarted} />
-      ) : (
-        <>
-          <h1>Welcome to the Home Page</h1>
-          <p>This is the home page content.</p>
-        </>
-      )}
+      <h1>Home</h1>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
