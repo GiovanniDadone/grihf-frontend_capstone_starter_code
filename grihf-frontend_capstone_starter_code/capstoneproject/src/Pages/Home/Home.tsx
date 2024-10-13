1481eaad365562d0c0931c965cc67c74576a2438
GiovanniDadone <dadone.giovanni94@gmail.com>
2024-10-14 01:17:48 +0200

    finally a functioning landing page

--- grihf-frontend_capstone_starter_code/capstoneproject/src/Pages/Home/Home.tsx
+++ grihf-frontend_capstone_starter_code/capstoneproject/src/Pages/Home/Home.tsx
@@ -1,10 +1,38 @@
+import { useState, useEffect } from 'react';
+import './Home.css';
+
 const Home = () => {
+  const [showLanding, setShowLanding] = useState(true);
+
+  useEffect(() => {
+    const hasSeenLanding = sessionStorage.getItem('hasSeenLanding');
+    if (hasSeenLanding) {
+      setShowLanding(false);
+    }
+  }, []);
+
+  const handleGetStarted = () => {
+    setShowLanding(false);
+    sessionStorage.setItem('hasSeenLanding', 'true');
+  };
 
   return (
     <div>
-      <h1>Home</h1>
+      {showLanding ? (
+        <div className="landing-page">
+          <h1>Welcome to Our Site</h1>
+          <button onClick={handleGetStarted}>Get Started</button>
+        </div>
+      ) : (
+        <div className="home-page">
+            <p>Instant Consultation</p>
+            <p>Book an Appointment</p>
+            <p>Self Check-up</p>
+            <p>Health Tips & Guidance</p>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
