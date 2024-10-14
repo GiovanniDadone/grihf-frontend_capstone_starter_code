6fa0347ecb00793b436b50e31ae9da59fe728ab7
GiovanniDadone <dadone.giovanni94@gmail.com>
2024-10-14 22:58:29 +0200

    self check up page added

---
+++ grihf-frontend_capstone_starter_code/capstoneproject/src/Pages/SelfCheckUp/SelfCheckUp.tsx
@@ -0,0 +1,10 @@
+const SelfCheckUp: React.FC = () => {
+  return (
+    <div>
+      <h1>Self Check-Up</h1>
+      <p>Take a self-check-up to see if you have any health issues.</p>
+      <button>Take Self Check-Up</button>
+    </div>
+  );
+};
+export default SelfCheckUp;
